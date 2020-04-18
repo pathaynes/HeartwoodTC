6c2a966570d0f66542ada734e2615e170ece985d
Pat Haynes <pathaynes32@gmail.com>
2020-04-18 17:09:14 -0600

    updated Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const Main = () => (
-  <h1>HEARTWOOD TREE CARE</h1>
-);
+const Main = () => {
+  return (
+    <h1>HEARTWOOD TREE CARE</h1>
+  );
+};
 
 export default Main;
